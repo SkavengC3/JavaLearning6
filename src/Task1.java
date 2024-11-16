1194b099879e8d44a2151509f61f521da7fa053d
SkavengC3 <egorlaptev760@gmail.com>
2024-11-16 11:43:10 +0200

    Create Task1.java

---
+++ src/Task1.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Task1 {
+    public static final int size = 8;
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int [] array = new int[size];
+        for(int i = 0; i < size; i++){
+            array[i] = random.nextInt(100);
+        }
+        int pare = 0;
+        int notpare = 0;
+        for (int num : array) {
+            if (num % 2 == 0) {
+                pare++;
+            } else {
+                notpare++;
+            }
+        }
+        System.out.println("Згенерований масив: ");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\nКількість парних чисел: " + pare);
+        System.out.println("Кількість непарних чисел: " + notpare);
+    }
+}
